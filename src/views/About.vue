d870a404cfaf03c565a064c7ac3971b9f0e8fe35
cjh <1418503647@qq.com>
2018-05-28 00:31:16 +0800

    backup

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,10 @@
 <template>
     <my-page title="关于">
         <div>BMI 计算器 v1.1.0</div>
+        isInFrame：{{ isInFrame }}
+        <div>
+           embed: {{ embed }} 
+        </div>
         <div class="copyright">© 2018 <a href="https://yunser.com" target="_blank">云设</a></div>
     </my-page>
 </template>
@@ -9,9 +13,13 @@
     export default {
         data () {
             return {
+                isInFrame: '',
+                embed: ''
             }
         },
         mounted() {
+            this.isInFrame = window.self !== window.top
+            this.embed = this.$route.query.embed === 'true' && this.isInFrame
         }
     }
 </script>
